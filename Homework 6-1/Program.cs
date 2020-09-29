e7ec9f5e22b99aa18bd07c4b9de154b1c72a7549
Upendra Samaranayake <humanfrailtz@gmail.com>
2020-09-28 17:09:11 -0700

    Add project files.

---
+++ Homework 6-1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Homework_6_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int heads = 0;
+            int tails = 0;
+            String toss;
+            Console.WriteLine("For each coin enter either h for heads or t for tails");
+
+            for (int i = 1; i <= 8; i++)
+            {
+                Console.WriteLine("Toss" + i + ":");
+                toss = Console.ReadLine();
+
+                if (toss.Equals("h"))
+                    {
+                    heads++;
+                        }
+                else
+                {
+                    tails++;
+                }
+
+
+                Console.WriteLine("Number of heads: " + heads);
+            Console.WriteLine("Number of tails: " + tails);
+            Console.WriteLine("Percent heads: " + (heads*100/8));
+            Console.WriteLine("Percent tailss: " + (tails*100/8));
+
+
+
+
+
+
+
+            }
+    }
+    }
+}
